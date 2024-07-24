d04ff779750c2f7c7c2a434ffc8115b928563603
Nastya Dolzhonok <nastenish89@gmail.com>
2024-07-24 16:17:25 +0300

    Ticket storage with 10 Tickets created

--- src/TicketService.java
+++ src/TicketService.java
@@ -1,21 +1,14 @@
 import java.time.Instant;
 
+import static java.time.Instant.*;
+
 public class TicketService {
     public static void main(String[] args) {
-        try {
-            Ticket ticketFull = new Ticket("4321", "Wembley", 333, Instant.now().getEpochSecond(), false, 'A', 5.0F);
-
-        } catch(IllegalArgumentException e) {
-            System.err.println(e.getMessage());
+        Ticket[] tickets = new Ticket[10];
+        for(int i = 0; i < 10; i++) {
+            tickets[i] = new Ticket();
         }
-
-        try {
-            Ticket ticketLimited = new Ticket("Wembley", 333, Instant.now().getEpochSecond());
-        } catch(IllegalArgumentException e) {
-            System.err.println(e.getMessage());
-        }
-
-        Ticket ticketEmpty = new Ticket();
 
     }
+
 }
